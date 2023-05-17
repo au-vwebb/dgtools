f5c47c6d211a4508f60d4a1bf57cd46f7e24a1fe
David Gamba <davidgamba@gmail.com>
2023-05-16 22:51:02 -0600

    tz: add colorschemes

--- tz/main.go
+++ tz/main.go
@@ -16,16 +16,24 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-var Logger = log.New(os.Stderr, "", log.LstdFlags)
+var Logger = log.New(io.Discard, "", log.LstdFlags)
 
 func main() {
 	os.Exit(program(os.Args))
 }
 
+// Time format used for printing
+// Choose between "15:04" or "03:04 PM"
+var HourMinuteFormat = "15:04"
+
+// TODO
+var HourFormat = "15"
+
 func program(args []string) int {
 	opt := getoptions.New()
 	opt.SetUnknownMode(getoptions.Pass)
-	opt.Bool("quiet", false, opt.GetEnv("QUIET"))
+	opt.Bool("verbose", false, opt.GetEnv("TZ_VERBOSE"))
+	opt.Bool("standard", false, opt.Alias("analog", "civilian", "12-hour", "12h", "am-pm"), opt.Description("Use standard 12 hour AM/PM time format"))
 	opt.SetCommandFn(Run)
 	opt.HelpCommand("help", opt.Alias("?"))
 
@@ -37,8 +45,12 @@ func program(args []string) int {
 		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
 		return 1
 	}
-	if opt.Called("quiet") {
-		Logger.SetOutput(io.Discard)
+	if opt.Called("verbose") {
+		Logger.SetOutput(os.Stderr)
+	}
+	if opt.Called("standard") {
+		HourMinuteFormat = "03:04 PM"
+		HourFormat = "03"
 	}
 	Logger.Println(remaining)
 
@@ -122,8 +134,7 @@ func ListRun(ctx context.Context, opt *getoptions.GetOpt, args []string) error {
 		}
 		count++
 	}
-	Logger.Printf("Total: %d", count)
-	fmt.Println()
+	Logger.Printf("Total: %d\n", count)
 
 	PrintActors(am)
 	return nil
@@ -143,19 +154,92 @@ func PrintActors(am ActorMap) {
 			count++
 			display = append(display, at.Display)
 		}
-		fmt.Printf("%s\n", strings.Join(display, ", "))
+		fmt.Printf("%s %s\n", am[offset][0].Time.Format(HourMinuteFormat), strings.Join(display, ", "))
 		PrintHours(am[offset][0].Time, am[offsets[0]][0].Time)
 	}
 	Logger.Printf("Total: %d", count)
 }
 
+type Palette struct {
+	Night     string
+	Dawn      string
+	Morning   string
+	Noon      string // work hours
+	AfterNoon string
+	Dusk      string
+	Evening   string
+
+	FgNight     string
+	FgDawn      string
+	FgMorning   string
+	FgNoon      string // work hours
+	FgAfterNoon string
+	FgDusk      string
+	FgEvening   string
+
+	Highlight   string
+	FgHighlight string
+}
+
+var PurpleYellow = Palette{
+	Night:     "#3d0066",
+	Dawn:      "#5c0099",
+	Morning:   "#c86bfa",
+	Noon:      "#fdc500",
+	AfterNoon: "#ffd500",
+	Dusk:      "#ffee32",
+	Evening:   "#03071e",
+}
+
+var BlueGreen = Palette{
+	Night:     "#003e7f",
+	Dawn:      "#0068af",
+	Morning:   "#5495e1",
+	Noon:      "#dddddd",
+	AfterNoon: "#44a75e",
+	Dusk:      "#008239",
+	Evening:   "#00540e",
+}
+
+var BlueYellow = Palette{
+	Night:     "#003e7f",
+	Dawn:      "#0068af",
+	Morning:   "#5495e1",
+	Noon:      "#dddddd",
+	AfterNoon: "#fef4d7",
+	Dusk:      "#f9b16e",
+	Evening:   "#00540e",
+
+	FgNight:     "#FFFFFF",
+	FgDawn:      "#FFFFFF",
+	FgMorning:   "#646970",
+	FgNoon:      "#000000",
+	FgAfterNoon: "#646970",
+	FgDusk:      "#FFFFFF",
+	FgEvening:   "#FFFFFF",
+
+	Highlight:   "#fafa00",
+	FgHighlight: "#000000",
+}
+
 func PrintHours(t, base time.Time) {
+	// p := PurpleYellow
+	// p := BlueGreen
+	p := BlueYellow
 	// Logger.Printf("t: %s", t.Format("-07"))
 	// Logger.Printf("base: %s", base.Format("-07"))
 	// Logger.Printf("last: %s", last.Format("-07"))
 	x := t.Hour()
 	h := t.Hour()
 	h -= 4
+
+	var hStyle = lipgloss.NewStyle().
+		Bold(true).
+		Foreground(lipgloss.Color(p.FgHighlight)).
+		Background(lipgloss.Color(p.Highlight)).
+		PaddingLeft(0).
+		PaddingRight(0)
+
 	for i := 0; i < 24; i++ {
 		if h >= 24 {
 			h = h - 24
@@ -164,20 +248,26 @@ func PrintHours(t, base time.Time) {
 			h = 24 + h
 		}
 		switch {
-		case h >= 0 && h < 3:
-			PrintBlock(fmt.Sprintf("%02d", h), "#FFFFFF", "#0A4B78", h == x)
-		case h >= 3 && h < 7:
-			PrintBlock(fmt.Sprintf("%02d", h), "#FFFFFF", "#135E96", h == x)
-		case h >= 7 && h < 9:
-			PrintBlock(fmt.Sprintf("%02d", h), "#646970", "#b8e6bf", h == x)
-		case h >= 9 && h < 15:
-			PrintBlock(fmt.Sprintf("%02d", h), "#000000", "#68de7c", h == x)
-		case h >= 15 && h < 17:
-			PrintBlock(fmt.Sprintf("%02d", h), "#646970", "#00ba37", h == x)
-		case h >= 17 && h < 22:
-			PrintBlock(fmt.Sprintf("%02d", h), "#FFFFFF", "#9ec2e6", h == x)
 		case h >= 22 && h < 24:
-			PrintBlock(fmt.Sprintf("%02d", h), "#FFFFFF", "#0A4B78", h == x)
+			PrintBlock(fmt.Sprintf("%02d", h), p.FgNight, p.Night, h == x, hStyle)
+		case h >= 0 && h < 3:
+			PrintBlock(fmt.Sprintf("%02d", h), p.FgNight, p.Night, h == x, hStyle)
+		case h >= 3 && h < 5:
+			PrintBlock(fmt.Sprintf("%02d", h), p.FgNight, p.Night, h == x, hStyle)
+		case h >= 5 && h < 7:
+			PrintBlock(fmt.Sprintf("%02d", h), p.FgDawn, p.Dawn, h == x, hStyle)
+		case h >= 7 && h < 9:
+			PrintBlock(fmt.Sprintf("%02d", h), p.FgMorning, p.Morning, h == x, hStyle)
+		case h >= 9 && h < 15:
+			PrintBlock(fmt.Sprintf("%02d", h), p.FgNoon, p.Noon, h == x, hStyle)
+		case h >= 15 && h < 17:
+			PrintBlock(fmt.Sprintf("%02d", h), p.FgAfterNoon, p.AfterNoon, h == x, hStyle)
+		case h >= 17 && h < 22:
+			PrintBlock(fmt.Sprintf("%02d", h), p.FgDusk, p.Dusk, h == x, hStyle)
+		case h >= 17 && h < 20:
+			PrintBlock(fmt.Sprintf("%02d", h), p.FgDusk, p.Dusk, h == x, hStyle)
+		case h >= 20 && h < 22:
+			PrintBlock(fmt.Sprintf("%02d", h), p.FgEvening, p.Evening, h == x, hStyle)
 		}
 		h++
 	}
@@ -185,14 +275,7 @@ func PrintHours(t, base time.Time) {
 
 }
 
-func PrintBlock(hour string, fg, bg string, highlight bool) {
-	var style = lipgloss.NewStyle().
-		Bold(true).
-		Foreground(lipgloss.Color("#FAFAFA")).
-		Background(lipgloss.Color("#7D56F4")).
-		PaddingLeft(0).
-		PaddingRight(0)
-
+func PrintBlock(hour string, fg, bg string, highlight bool, hStyle lipgloss.Style) {
 	var normal = lipgloss.NewStyle().
 		Foreground(lipgloss.Color(fg)).
 		Background(lipgloss.Color(bg)).
@@ -201,7 +284,7 @@ func PrintBlock(hour string, fg, bg string, highlight bool) {
 
 	if highlight {
 		// hour = fmt.Sprintf("%s", aurora.Index(0, aurora.BgIndex(229, hour)))
-		hour = style.Render(hour)
+		hour = hStyle.Render(hour)
 	} else {
 		// hour = fmt.Sprintf("%s", aurora.Index(fg, aurora.BgIndex(bg, hour)))
 		hour = normal.Render(hour)
